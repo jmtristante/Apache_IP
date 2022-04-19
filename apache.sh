f98ec2273595b688dc6ae0561928bd1529d40c4b
jmtristante <jmtristante@protonmail.com>
2022-04-19 16:42:18 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-fichero="recortado.log"
+#fichero="recortado.log"
 regexIP='^([0-9]{1,3}\.){3}[0-9]{1,3}' #Expresion regular para filtrar IPs
 
 if [ -n "$1" ]; then
 	fichero=$1
+else
+	echo "Proporciona fichero de log"
+	exit 1
 fi
 
 #Delaramos un hashmap
 declare -A map
 
+function show_map(){
+	#Se muestra el map
+	echo -ne "\nSe han analizado $ilines lineas de log\n"
+	for k in "${!map[@]}"; do
+	        printf "%-18s%s\n" $k  ${map[$k]}
+	done
+}
+
+
 #Para el porcentaje de completitud
-nlines=$(wc -l < $fichero)
-ilines=0
+nlines=$(wc -l < $fichero) 	#Lineas totales del fichero
+ilines=0			#Lineas analizadas
 BAR='####################'
 SPACES='                   '
+
+
+trap ctrl_c INT
+function ctrl_c() {
+	show_map
+	exit 0
+}
 
 #Recorremos el fichero, extrayendo las IP e incluyendolas en el map. Si ya existen se suma 1 a su contador
 while IFS='' read -r linea || [[ -n "$linea" ]]; do
@@ -36,7 +55,5 @@ while IFS='' read -r linea || [[ -n "$linea" ]]; do
 done < $fichero
 
 #Se muestra el map
-echo
-for k in "${!map[@]}"; do 
-	printf "%-18s%s\n" $k  ${map[$k]}
-done
+show_map
+
